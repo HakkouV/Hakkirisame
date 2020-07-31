52d0546fd4d0b6ae4536fc51fb8adbb9db55feea
Hakkou <68609910+HakkouV@users.noreply.github.com>
2020-07-31 04:25:12 -0300

    adicionando o bot

---
+++ hakkirisame.py
@@ -0,0 +1,33 @@
+import random
+import discord
+from discord.ext import commands
+
+hakkiri = commands.Bot(command_prefix = '$')
+
+# Events
+@hakkiri.event
+async def on_ready():
+    print('Bot is online')
+    await hakkiri.change_presence(activity=discord.Game('6 castanhas no quarto de jayb'))
+
+# Commands
+@hakkiri.command(aliases=['ajuda'])
+async def _help(ctx):
+    await ctx.send('lista de comandos aqui: https://github.com/HakkouV/Hakkirisame')
+@hakkiri.command()
+async def ping(ctx):
+    pongs = ['pong no teu bong?????', 'pong o caralho te fode fidaputa', 'pong é massa']
+    await ctx.send(f'{random.choice(pongs)}')
+
+@hakkiri.command(aliases=['6ball', 'responda'])
+async def _8ball(ctx, *, question):
+    respostas = ['sim', 'com ctz', 'se pa', 'sla porra', 'é 6', 'claro que n mamao', 'te fode fidaputa']
+    await ctx.send(f'{random.choice(respostas)}')
+
+@hakkiri.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, *, amount=5):
+    await ctx.channel.purge(limit=amount)
+
+
+hakkiri.run('...')
